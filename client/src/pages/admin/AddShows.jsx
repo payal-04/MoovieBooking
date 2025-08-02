58f007a6aa9782eb3b8628d6891aee6dc30b7287
payal-04 <payal078601payal@gmail.com>
2025-08-02 20:16:03 +0530

    admin commited

--- client/src/pages/admin/AddShows.jsx
+++ client/src/pages/admin/AddShows.jsx
@@ -1,6 +1,25 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { dummyShowsData } from '../../assets/assets';
 
 const AddShows = () => {
+
+const currency = import.meta.env.VITE_CURRENCY 
+
+const[nowPlayingMovies, setNowPlayingMovies] = useState([])
+const[selectMovies, setSelectedMovie] = useState(null);
+const [dateTimeSelection, setDateTimeSelection] = useState({});
+const[dateTimeInput, setDateTimeInput] = useState("");
+const[showPrice, setShoePrice] = useState("");
+
+const fetchNowPlayingMovies = async()=>{
+  setNowPlayingMovies(dummyShowsData)
+};
+
+useEffect(()=>{
+  fetchNowPlayingMovies();
+},[])
+
+
   return (
     <div>
       
