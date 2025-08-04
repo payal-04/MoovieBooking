edf3b671b8d89b50f01c70133569cff4fbd33714
payal-04 <payal078601payal@gmail.com>
2025-08-04 19:51:20 +0530

    index commited

--- server/inngest/index.js
+++ server/inngest/index.js
@@ -17,8 +17,41 @@ const userData = {
      name: first_name+' '+last_name, 
      image: image_url 
 } 
-await User.create(userData) } )
+await User.create(userData) 
+} 
+)
+
+//Inngest Function to delete user from Database//
+const syncUserDeletion = inngest.createFunction( 
+{id: 'delete-user-from-clerk'}, 
+{event: 'clerk/user.deleted'}, 
+async({event})=>{ 
+      
+     const {id} = event.data
+     await User.findByIdAndDelete(id)
+   } 
+ )
+
+ //Inngest Function to update user data in database//
+ const syncUserUpdation = inngest.createFunction( 
+{id: 'update-user-from-clerk'}, 
+{event: 'clerk/user.updated'}, 
+async({event})=>{ 
+      
+    const{id, first_name, last_name, email_addresses, image_url} = event.data 
+
+    const userData = { 
+    _id: id, 
+     email: email_addresses[0].email_address, 
+     name: first_name+' '+last_name, 
+     image: image_url 
+   } 
+   } 
+ )
 
 
 // Create an empty array where we'll export future Inngest functions 
-export const functions = [syncUserCreation];
+export const functions = [
+    syncUserCreation,
+    syncUserDeletion
+];
