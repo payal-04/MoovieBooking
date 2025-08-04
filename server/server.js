c24ab904d13d99107ed40d7c6d8f92f84b025abf
payal-04 <payal078601payal@gmail.com>
2025-08-04 19:22:20 +0530

    server commited

--- server/server.js
+++ server/server.js
@@ -3,6 +3,8 @@ import cors from 'cors';
 import 'dotenv/config';
 import connectDB from './configs/db.js';
 import {clerkMiddleware} from '@clerk/express'
+import { serve } from "inngest/express";
+import { inngest, functions } from "./inngest/index.js"
 
 const app = express();
 const port = 3000;
@@ -17,5 +19,6 @@ app.use(clerkMiddleware())
 
 //API Routes//
 app.get('/', (req,res)=> res.send("Server is Live"))
+app.use("/api/inngest", serve({ client: inngest, functions }));
 
 app.listen(port, ()=> console.log(`Server is Listening at http://localhost:${port}`))
