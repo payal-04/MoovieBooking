d53345e72ea130e9486c739376f324f1f03169bd
payal-04 <payal078601payal@gmail.com>
2025-08-06 19:10:47 +0530

    commited

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -6,9 +6,10 @@ export const protectAdmin = async (req,res, next) => {
 
         const user = await clerkClient.users.getUser(userId)
 
+
        if(user.privateMetadata.role !== 'admin'){
           return res.json({success: false, message: "Not Authorized"})
-       }
+       } 
 
        next();
 
